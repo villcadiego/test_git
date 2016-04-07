8aef55acc4d80bc2ccb1aa31b314d6ec22353d75
DIEGO <DIEGO@192.168.0.16>
2016-04-07 01:54:21 -0300

    Se va a comitear esto q se mersheo!

--- evaluacion_tecnica2/src/dvillca/dao/PedidosDAO.java
+++ evaluacion_tecnica2/src/dvillca/dao/PedidosDAO.java
@@ -3,11 +3,13 @@ package dvillca.dao;
 import dvillca.model.Pedido;
 
 /**
+ * modi
+ * Se edita desde el administrador!
  * @author dvillca
  * Interface que me proporciona el Ejercicio
  */
 public interface PedidosDAO {
-	void insertOrUpdate(Pedido pedido);	// 	inserta un nuevo pedido en la base de datos o modifica un pedido existente (en cado de crear uno nuevo, el pedido pasado como par·metro se completa con el nuevo id).
+	void insertOrUpdate(Pedido pedido);	// 	inserta un nuevo pedido en la base de datos o modifica un pedido existente (en cado de crear uno nuevo, el pedido pasado como par√°metro se completa con el nuevo id).
 	void delete(Pedido pedido);			// 	elimina el pedido que corresponde al id recibido.
 	Pedido select(Integer idPedido); 	//	busca un pedido por id.
 }
