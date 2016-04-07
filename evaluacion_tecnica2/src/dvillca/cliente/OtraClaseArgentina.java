a33940a920590301c1563ce62b86441e1dc14c3e
DIEGO <DIEGO@192.168.0.16>
2016-04-07 02:08:18 -0300

    nueva clase

---
+++ evaluacion_tecnica2/src/dvillca/cliente/OtraClaseArgentina.java
@@ -0,0 +1,13 @@
+package dvillca.cliente;
+
+public class OtraClaseArgentina {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
